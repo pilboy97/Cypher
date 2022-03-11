a219cf2f524b90a1663df411076914589fa7ab79
pilboy97 <76037604+pilboy97@users.noreply.github.com>
2022-03-11 23:07:17 +0900

    add comment

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 )
 
+//Input a name of cipher algorithm
 func AskAlgorithm() string {
 	var buf string
 	for {
@@ -20,6 +21,8 @@ func AskAlgorithm() string {
 		}
 	}
 }
+
+//Input a word
 func AskWord() string {
 	rexp := regexp.MustCompile("[[:lower:]]+")
 	var buf string
@@ -31,12 +34,16 @@ func AskWord() string {
 		}
 	}
 }
+
+//Input a letter
 func AskChar() byte {
 	var buf byte
 	fmt.Print("Enter Letter (written in lowercase): ")
 	fmt.Scanln(&buf)
 	return buf
 }
+
+//Check the given number is prime
 func IsPrime(num int) bool {
 	if num < 2 {
 		return false
@@ -49,6 +56,8 @@ func IsPrime(num int) bool {
 
 	return true
 }
+
+//Input a prime number less than 50
 func AskPrime() int {
 	var num int
 	for {
@@ -59,6 +68,8 @@ func AskPrime() int {
 		}
 	}
 }
+
+//Input a number between a and b
 func AskNum(a, b int) int {
 	var num int
 	if a > b {
@@ -74,11 +85,14 @@ func AskNum(a, b int) int {
 	}
 }
 
+//Explain Caesar Algorithm
 func Caesar() {
 	fmt.Println("Caesar cipher")
 	fmt.Println("This algorithm replaces each letter with the letter after nth in the alphabet")
 
+	//input a word
 	str := AskWord()
+	//input a key number
 	key := AskNum(1, 25)
 
 	fmt.Println("How to shift alphabet with the given key")
@@ -88,7 +102,9 @@ func Caesar() {
 
 	result := []byte(str)
 	for i := range result {
+		//letter is the ith letter
 		letter := result[i]
+		//replace with the letter after the given key in the alphabet
 		result[i] = byte((int(letter-'a')+key)%26 + 'a')
 	}
 
